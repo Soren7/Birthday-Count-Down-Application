79a0a331500a0533b0b0a42271b8274d84300e06
Maxwell Lewis <maxsolewis@gmail.com>
2018-07-25 14:22:15 +0000

    styled with CSS grid

--- src/components/clock.js
+++ src/components/clock.js
@@ -4,21 +4,21 @@ class Clock extends Component {
     render() {
         return (
             <div className="clock">
-                <div className="clock__days"> 
+                <div className="clock__days clock__box"> 
                     <lable className="clock__title">DAYS </lable>
-                    <lable className="clock__amount">25</lable>
+                    <lable className="clock__amount">300</lable>
                 </div>
-                <div className="clock__hours"> 
+                <div className="clock__hours clock__box"> 
                     <lable className="clock__title">HOURS </lable>
-                    <lable className="clock__amount">25</lable>
+                    <lable className="clock__amount">16</lable>
                 </div>
-                <div className="clock__minutes"> 
+                <div className="clock__minutes clock__box"> 
                     <lable className="clock__title">MINUTES </lable>
-                    <lable className="clock__amount">25</lable>
+                    <lable className="clock__amount">42</lable>
                 </div>
-                <div className="clock__seconds"> 
+                <div className="clock__seconds clock__box"> 
                     <lable className="clock__title">SECONDS </lable>
-                    <lable className="clock__amount">25</lable>
+                    <lable className="clock__amount">22</lable>
                 </div>
             </div>    
         );
